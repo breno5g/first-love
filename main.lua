2fb5b0a1ea5019ac1e0f88904b3fb29e4142acf5
Breno Santos <brenosantos145@gmail.com>
2024-12-18 16:09:24 -0300

    feat: new params for class

--- main.lua
+++ main.lua
@@ -3,15 +3,17 @@ function love.load()
 	--Don't forget to load the file
 	require("rectangle")
 
-	r1 = Rectangle()
-	r2 = Rectangle()
+	r1 = Rectangle(100, 100, 200, 50, 100)
+	r2 = Rectangle(350, 80, 25, 140, 120)
 	print(r1.test, r2.test)
 end
 
 function love.update(dt)
 	r1:update(dt)
+	r2:update(dt)
 end
 
 function love.draw()
 	r1:draw()
+	r2:draw()
 end
--- rectangle.lua
+++ rectangle.lua
@@ -1,11 +1,11 @@
 Rectangle = Object.extend(Object)
 
-function Rectangle:new()
-	self.x = 100
-	self.y = 100
-	self.width = 200
-	self.height = 150
-	self.speed = 100
+function Rectangle:new(x, y, width, height, speed)
+	self.x = x
+	self.y = y
+	self.width = width
+	self.height = height
+	self.speed = speed
 end
 
 function Rectangle:update(dt)
