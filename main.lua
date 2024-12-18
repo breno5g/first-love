820f7d2637852baf1e4650ffac9d0ec991e356f0
Breno Santos <brenosantos145@gmail.com>
2024-12-18 13:22:36 -0300

    feat: improve controls to move rectangle

--- main.lua
+++ main.lua
@@ -1,14 +1,16 @@
 function love.load()
-	x = 50
+	x = 300
+	y = 150
 	speed = 100
 end
 
 function love.update(dt)
-	if x < 600 then
-		x = x + (speed * dt)
-	end
+	local xDir = love.keyboard.isDown("right") and 1 or love.keyboard.isDown("left") and -1 or 0
+	local yDir = love.keyboard.isDown("up") and -1 or love.keyboard.isDown("down") and 1 or 0
+	x = x + speed * dt * xDir
+	y = y + speed * dt * yDir
 end
 
 function love.draw()
-	love.graphics.rectangle("line", x, 50, 200, 150)
+	love.graphics.rectangle("line", x, y, 200, 150)
 end
