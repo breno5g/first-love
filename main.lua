ba1b45c6082a1d53398154bd20c85107fa7a21c7
Breno Santos <brenosantos145@gmail.com>
2024-12-18 12:30:58 -0300

    feat: draw a rectangle

--- main.lua
+++ main.lua
@@ -1,3 +1,8 @@
-function love.draw() 
-  love.graphics.print("Hello, World!", 100, 100)
+function rectangle()
+	love.graphics.rectangle("fill", 100, 200, 50, 80)
+end
+
+function love.draw()
+	-- love.graphics.print("Hello, World!", 100, 100)
+	rectangle()
 end
