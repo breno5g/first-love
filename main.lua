e8a4eea043025fa8a24feee1e6dbd01e962b72f4
Breno Santos <brenosantos145@gmail.com>
2024-12-20 13:19:07 -0300

    feat: trying to use a math functions to move a arrow

--- main.lua
+++ main.lua
@@ -38,6 +38,7 @@
 -- end
 
 local circle = {}
+local arrow = {}
 
 function love.load()
 	love.window.setMode(1000, 800)
@@ -45,7 +46,18 @@ function love.load()
 	circle.x = 100;
 	circle.y = 100;
 	circle.radius = 50;
-	circle.speed = 200
+	circle.speed = 200;
+
+	arrow.x = 200;
+	arrow.y = 200;
+	arrow.speed = 200;
+	arrow.angle = 0;
+	arrow.image = love.graphics.newImage("assets/arrow_right.png")
+	arrow.origin_x, arrow.origin_y = arrow.image:getDimensions()
+
+	arrow.origin_x = arrow.origin_x / 2
+	arrow.origin_y = arrow.origin_y / 2
+
 end
 
 function love.keypressed(key)
@@ -65,28 +77,42 @@ end
 function love.update(dt)
 	mouse_x, mouse_y = love.mouse.getPosition()
 
-	angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
+	-- Use this for circle
+	-- angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
 
-	cos = math.cos(angle)
-	sin = math.sin(angle)
+	-- cos = math.cos(angle)
+	-- sin = math.sin(angle)
 
-	local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+	-- local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
 
-	if distance < 400 then
-			circle.x = circle.x + circle.speed * cos * (distance/100) * dt
-			circle.y = circle.y + circle.speed * sin * (distance/100) * dt
-	end
+	-- if distance < 400 then
+	-- 		circle.x = circle.x + circle.speed * cos * (distance/100) * dt
+	-- 		circle.y = circle.y + circle.speed * sin * (distance/100) * dt
+	-- end
+
+	-- Use this for arrow
+	angle = math.atan2(mouse_y - arrow.y, mouse_x - arrow.x)
+	arrow.angle = angle
+
+	arrow.x = arrow.x + arrow.speed * math.cos(angle) * dt
+	arrow.y = arrow.y + arrow.speed * math.sin(angle) * dt
+
+
 end
 
 function love.draw()
 	love.graphics.print("angle: " .. angle, 10, 10)
-	love.graphics.circle("line", circle.x, circle.y, circle.radius)
+	-- love.graphics.circle("line", circle.x, circle.y, circle.radius)
 
 
-	love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
-	love.graphics.line(mouse_x, mouse_y, mouse_x, circle.y)
-	love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
+	-- love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+	-- love.graphics.line(mouse_x, mouse_y, mouse_x, circle.y)
+	-- love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
 
-	local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
-	love.graphics.circle("line", circle.x, circle.y, distance)
+	-- local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+	-- love.graphics.circle("line", circle.x, circle.y, distance)
+
+	love.graphics.circle("fill", mouse_x, mouse_y, 5)
+
+	love.graphics.draw(arrow.image, arrow.x, arrow.y, arrow.angle, 1, 1, arrow.origin_x, arrow.origin_y)
 end
