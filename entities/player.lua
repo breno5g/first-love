82734da11a87f130095329fa252b63ff8e7ce875
Breno Santos <brenosantos145@gmail.com>
2024-12-19 21:36:42 -0300

    feat: add movement

--- entities/player.lua
+++ entities/player.lua
@@ -5,10 +5,23 @@ function Player:new()
 	self.x = 300
 	self.y = 20
 	self.speed = 500
+	self.width = self.image:getWidth()
 end
 
 function Player:draw()
 	love.graphics.draw(self.image, self.x, self.y)
+end
+
+function Player:update(dt)
+	local xDir = (love.keyboard.isDown("right") and 1 or 0) - (love.keyboard.isDown("left") and 1 or 0)
+	self.x = self.x + self.speed * dt * xDir
+
+	local window_width = love.graphics.getWidth()
+	if self.x < 0 then
+		self.x = 0
+	elseif self.x + self.width > window_width then
+		self.x = window_width - self.width
+	end
 end
 
 return Player
--- main.lua
+++ main.lua
@@ -5,7 +5,9 @@ function love.load()
 	player = Player()
 end
 
-function love.update(dt) end
+function love.update(dt)
+	player:update(dt)
+end
 
 function love.draw()
 	player:draw()
