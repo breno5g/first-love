c2d97ad0a13ab1f7bb57c9aa955b9be65801849a
Breno Santos <brenosantos145@gmail.com>
2025-01-17 11:46:23 -0300

    feat: draw in cells

--- main.lua
+++ main.lua
@@ -2,10 +2,18 @@ local cellSize = 5
 local selectedX = 0
 local selectedY = 0
 local gridXCount, gridYCount = 500, 500
+local grid = {}
 
 function love.load()
 	love.window.setMode(500, 500)
 	love.graphics.setBackgroundColor(1, 1, 1)
+
+	for y = 1, gridYCount do
+		grid[y] = {}
+		for x = 1, gridXCount do
+			grid[y][x] = false
+		end
+	end
 end
 
 function love.keypressed(key)
@@ -17,6 +25,10 @@ end
 function love.update()
 	selectedX = math.min(math.floor(love.mouse.getX() / cellSize) + 1, gridXCount)
 	selectedY = math.min(math.floor(love.mouse.getY() / cellSize) + 1, gridYCount)
+
+	if love.mouse.isDown(1) then
+		grid[selectedY][selectedX] = true
+	end
 end
 
 function love.draw()
@@ -26,6 +38,8 @@ function love.draw()
 
 			if x == selectedX and y == selectedY then
 				love.graphics.setColor(0, 1, 1)
+			elseif grid[y][x] then
+				love.graphics.setColor(1, 0, 1)
 			else
 				love.graphics.setColor(0.86, 0.86, 0.86)
 			end
